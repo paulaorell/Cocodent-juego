e4cbe00af9b03ccb6e0dd56f06defbf1750ef15a
Pau2055 <hpaulina042@gmail.com>
2025-02-19 09:59:06 -0500

    xd

--- Assets/Scripts/PersistentObject.cs
+++ Assets/Scripts/PersistentObject.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PersistentObject : MonoBehaviour
 {
@@ -13,6 +14,16 @@ public class PersistentObject : MonoBehaviour
         }
         else if (instance != this)
         {
+            Destroy(gameObject);
+        }
+    }
+
+     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Verificar si la escena cargada es "MenuPrincipal"
+        if (scene.name == "MenuPrincipal")
+        {
+            // Destruir el objeto persistente cuando se carga la escena MenuPrincipal
             Destroy(gameObject);
         }
     }
